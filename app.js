51e84cfc01a635922aa1e4343350d8699edf0d69
pellehardstedt <pelle.hardstedt>
2021-03-04 21:24:45 +0100

    datepicker on frontend, formatting requests on backend

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var requests = require('request')
-var app = express();
+const express = require('express');
+const requests = require('request')
+const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
+const { json } = require('express');
 
-const singlePrediction =  {
-    "instances": [
-        {
-            "features": ["2012-12-19 17:00:00",4,0,1,1,16.4,20.455,50,26.0027]
-            
-        }
-    ]
-}
+const allDates =require(path.join(__dirname+'/private/allDates.json'));
 
-var keys = require(path.join(__dirname+'/private/keys.json');
+const keys = require(path.join(__dirname+'/private/keys.json'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,23 +15,56 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.get('/bike_predict',function(req,res){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/bike_predict',function(req,res){
+    dateData = searchDate(req.body.data)
+
+    dateJSON = `{"instances": [{"features": ${dateData}}]}`
     var toFrontend;
     const options = {
         url: keys.url,
         json: true,
-        body: singlePrediction
+        body: dateJSON
     };
 
     requests.post(options, (err, response, body) => {
         if (err) {
-            return console.log(err);
+            return console.log(err)
         }
         //responde to the outer request
-        res.json(body)
+        res.json(dateJSON)
     });
 });
 
-app.listen(80, function(){
-    console.log("Listening on port 80")
+app.listen(3000, function(){
+    console.log("Listening on port 3000")
 });
+
+function searchDate(date){
+    let returnCSV;
+    for(let i in allDates){
+        if(allDates[i].datetime == date){
+            returnCSV = allDates[i]
+            break
+        }
+    }
+    jsonString = JSON.stringify(returnCSV)
+    jsonArray = jsonString.split('":"')
+
+    date = jsonArray[1].split(',"')[0]
+    date = date.substring(0, date.length - 1);
+    date = '"' + date + '"';
+    jsonArray = jsonArray[1].split('":')
+    holiday = jsonArray[1].split(',"')[0]
+    workingday = jsonArray[2].split(',"')[0]
+    weather = jsonArray[3].split(',"')[0]
+    temp = jsonArray[4].split(',"')[0]
+    atemp = jsonArray[5].split(',"')[0]
+    humidity = jsonArray[6].split(',"')[0]
+    windspeed = jsonArray[7].split(',"')[0]
+
+    return [date, holiday, workingday, weather, temp, atemp, humidity, windspeed]
+}
--- public/index.js
+++ public/index.js
@@ -1,14 +1,109 @@
 $(function(){
-
+    $('#dateInput').datetimepicker({
+        ownerDocument: document,
+        contentWindow: window,
+        
+        value: '',
+        rtl: false,
+        
+        format: 'Y/m/d H:i',
+        formatTime: 'H:i',
+        formatDate: 'Y/m/d',
+        
+        startDate:  false, // new Date(), '1986/12/08', '-1970/01/05','-1970/01/05',
+        step: 60,
+        monthChangeSpinner: true,
+        
+        closeOnDateSelect: false,
+        closeOnTimeSelect: true,
+        closeOnWithoutClick: true,
+        closeOnInputClick: true,
+        openOnFocus: true,
+        
+        timepicker: true,
+        datepicker: true,
+        weeks: false,
+        
+        defaultTime: '12:00', // use formatTime format (ex. '10:00' for formatTime: 'H:i')
+        defaultDate: '2011/01/01', // use formatDate format (ex new Date() or '1986/12/08' or '-1970/01/05' or '-1970/01/05')
+        
+        minDate: '2011/01/01',
+        maxDate: '2012-12-31',
+        minTime: false,
+        maxTime: false,
+        minDateTime: false,
+        maxDateTime: false,
+        
+        allowTimes: [],
+        opened: false,
+        initTime: true,
+        inline: false,
+        theme: '',
+        touchMovedThreshold: 5,
+        
+        onSelectDate: function () {},
+        onSelectTime: function () {},
+        onChangeMonth: function () {},
+        onGetWeekOfYear: function () {},
+        onChangeYear: function () {},
+        onChangeDateTime: function () {},
+        onShow: function () {},
+        onClose: function () {},
+        onGenerate: function () {},
+        
+        withoutCopyright: true,
+        inverseButton: false,
+        hours12: false,
+        next: 'xdsoft_next',
+        prev : 'xdsoft_prev',
+        dayOfWeekStart: 0,
+        parentID: 'body',
+        timeHeightInTimePicker: 25,
+        
+        todayButton: false,
+        prevButton: true,
+        nextButton: true,
+        defaultSelect: true,
+        
+        scrollMonth: true,
+        scrollTime: true,
+        scrollInput: true,
+        
+        lazyInit: false,
+        mask: false,
+        validateOnBlur: true,
+        allowBlank: true,
+        yearStart: '2011',
+        yearEnd: '2012',
+        monthStart: 0,
+        monthEnd: 11,
+        style: '',
+        id: '',
+        fixed: false,
+        roundTime: 'round', // ceil, floor
+        className: '',
+        weekends: [],
+        highlightedDates: [],
+        highlightedPeriods: [],
+        allowDates : ['2011/01/01', '2011/01/20', '2011/01/21','2011/01/22','2011/01/23','2011/01/24','2011/01/25','2011/01/26','2011/01/27','2011/01/28','2011/01/29','2011/01/30'],
+        allowDateRe : null,
+        disabledDates : [],
+        disabledWeekDays: [],
+        yearOffset: 0,
+        beforeShowDay: null,
+        
+        enterLikeTab: true,
+        showApplyButton: false
+        });
 });
 
 function btnPredict() {
-    $.getJSON('/bike_predict',function(json){
-        console.log(json.body)
+    let date = $('#dateInput').datetimepicker('getValue')
+    date = date.toLocaleString();
+    $.post('/bike_predict', {data: date}, function(json){
         $('.result').append(
             '<li><a href="#">' + json.body + '</a></li>'
         );
-    });
+        $('#dateInput').datetimepicker('reset');
+    }, 'json');
 }
-
- 
