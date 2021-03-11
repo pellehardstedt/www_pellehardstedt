9b99a4a0d2126c55d376ef6395cd101a004a2e95
pellehardstedt <pelle.hardstedt>
2021-03-11 09:29:56 +0100

    redirect

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+app.get('*', (req, res) => {
+    console.log('redirecting')
+    res.redirect('/');
+});
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
